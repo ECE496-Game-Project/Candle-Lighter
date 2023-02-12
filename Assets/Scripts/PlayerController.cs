5404a69930a7683b2503eb75926ffd5e9e7e5f64
Yang Chen <jackchen.chen@mail.utoronto.ca>
2023-02-12 00:55:44 -0500

    upload final

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
-using UnityEditor.Experimental.GraphView;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Assets.Scripts.Light;
