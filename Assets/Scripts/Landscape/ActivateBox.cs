dbcdd798c71aedb564497a13dda00b27042e6ab6
Yang Chen <jackchen.chen@mail.utoronto.ca>
2023-02-11 21:21:49 -0500

    enable elevator trigger

--- Assets/Scripts/Landscape/ActivateBox.cs
+++ Assets/Scripts/Landscape/ActivateBox.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using UnityEngine;
 using Assets.Scripts.Light;
+
 namespace Assets.Scripts.Landscape {
     public class ActivateBox : BaseLandscape {
+        
 
+        public Animator _Elevator_Animator;
         public override void ActivateExecute() {
+            if (!_Elevator_Animator.GetBool("isActivated"))
+                _Elevator_Animator.SetBool("isActivated", true);
+
+            else
+                _Elevator_Animator.SetBool("isActivated", false);
             
         }
     }
